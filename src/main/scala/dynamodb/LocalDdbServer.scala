47e24aff63ce6da35f8b949b7aeb865e6156c0a6
avinder.bahra <avinder.bahra@digital.hmrc.gov.uk>
2021-04-29 06:55:23 +0100

    layer simplification

--- src/main/scala/dynamodb/LocalDdbServer.scala
+++ src/main/scala/dynamodb/LocalDdbServer.scala
@@ -2,41 +2,25 @@ package dynamodb
 
 import com.amazonaws.services.dynamodbv2.local.main.ServerRunner
 import com.amazonaws.services.dynamodbv2.local.server.DynamoDBProxyServer
-import zio.{Has, UIO, ZIO, ZLayer}
+import zio.blocking.{effectBlocking, Blocking}
+import zio.{Has, UIO, ZIO, ZLayer, ZManaged}
 
 object LocalDdbServer {
-  type LocalDdbServer = Has[Service]
 
-  trait Service {
-    def start: UIO[Unit]
-  }
+  val inMemoryLayer: ZLayer[Blocking, Throwable, Has[DynamoDBProxyServer]] =
+    (ZManaged.make {
+      effectBlocking {
+        System.setProperty("sqlite4java.library.path", "native-libs")
+        System.setProperty("aws.accessKeyId", "dummy-key")
+        System.setProperty("aws.secretKey", "dummy-key") // This is not used
+        System.setProperty("aws.secretAccessKey", "dummy-key")
 
-  private val inMemoryServer: ZLayer[Any, Nothing, Has[DynamoDBProxyServer]] =
-    ZLayer.fromAcquireRelease[Any, Nothing, DynamoDBProxyServer] {
-      UIO(println("Creating local DynamoDb server")) *> UIO(createServer)
-    } { server =>
-      UIO(println("Stopping local DynamoDb server")) *> UIO(server.stop())
-    }
+        // Create an in-memory and in-process instance of DynamoDB Local that runs over HTTP
 
-  val live: ZLayer[Any, Nothing, Has[Service]] = inMemoryServer >>> ZLayer
-    .fromService[DynamoDBProxyServer, LocalDdbServer.Service] { server =>
-      new LocalDdbServer.Service {
-        override def start: UIO[Unit] = UIO(server.start())
+        val localArgs = Array("-inMemory")
+        val server: DynamoDBProxyServer = ServerRunner.createServerFromCommandLineArgs(localArgs)
+        server.start()
+        server
       }
-    }
-
-  def start: ZIO[LocalDdbServer, Nothing, Unit] = ZIO.accessM[LocalDdbServer](_.get.start)
-
-  private def createServer: DynamoDBProxyServer = {
-    System.setProperty("sqlite4java.library.path", "native-libs")
-    System.setProperty("aws.accessKeyId", "dummy-key")
-    System.setProperty("aws.secretKey", "dummy-key") // This is not used
-    System.setProperty("aws.secretAccessKey", "dummy-key")
-
-    // Create an in-memory and in-process instance of DynamoDB Local that runs over HTTP
-
-    val localArgs = Array("-inMemory")
-    val server: DynamoDBProxyServer = ServerRunner.createServerFromCommandLineArgs(localArgs)
-    server
-  }
+    }(server => effectBlocking(server.stop()).orDie)).toLayer
 }
--- src/test/scala/dynamodb/StreamingQuerySpec.scala
+++ src/test/scala/dynamodb/StreamingQuerySpec.scala
@@ -1,16 +1,20 @@
 package dynamodb
 
+import com.amazonaws.services.dynamodbv2.local.server.DynamoDBProxyServer
 import dynamodb.AwsLayers.ddbLayer
 import dynamodb.Requests._
-import io.github.vigoo.zioaws.dynamodb.{createTable, putItem, query}
-import zio.ZIO
+import io.github.vigoo.zioaws.core.AwsError
+import io.github.vigoo.zioaws.dynamodb.model.AttributeValue
+import io.github.vigoo.zioaws.dynamodb.model.primitives.AttributeName
+import io.github.vigoo.zioaws.dynamodb.{createTable, putItem, query, DynamoDb}
 import zio.console._
 import zio.test.Assertion.equalTo
 import zio.test.{DefaultRunnableSpec, suite, _}
+import zio.{Chunk, ZIO}
 
 object StreamingQuerySpec extends DefaultRunnableSpec {
 
-  val program = for {
+  val program: ZIO[DynamoDb, AwsError, Chunk[Map[AttributeName, AttributeValue.ReadOnly]]] = for {
     _ <- createTable(createTableRequest)
     _ <- ZIO.foreach_(1 to 5) { i =>
       putItem(putItemRequest(i))
@@ -23,15 +27,13 @@ object StreamingQuerySpec extends DefaultRunnableSpec {
     suite(label = "StreamingQuerySpec")(
       testM("should return the results of a streamed query") {
         val value = (for {
-          svr <- ZIO.service[LocalDdbServer.Service]
-          _ <- svr.start
+          _ <- ZIO.service[DynamoDBProxyServer]
           items <- program
           rowCount = items.size
           _ <- putStrLn(s"total rows found=${items.size}")
           _ <- putStrLn(s"${items.toList}")
         } yield assert(rowCount)(equalTo(5)))
-          .provideCustomLayer(ddbLayer ++ LocalDdbServer.live)
         value
-      }
+      }.provideCustomLayerShared((ddbLayer ++ LocalDdbServer.inMemoryLayer).orDie)
     )
 }
